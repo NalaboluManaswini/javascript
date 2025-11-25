6ed5e0d51ddbef48606afd995cf29ff249ff6d96
NalaboluManaswini <manaswininalabolu56@gmail.com>
2025-11-25 15:39:21 +0530

    1opertor

---
+++ operators/app.js
@@ -0,0 +1,213 @@
+// let num1 = Number(prompt("Enter the number1"));
+// let num2 = Number(prompt("Enter the number2"));1
+// alert()
+// Arithmetic Practice Questions
+
+// What is the result of 15 + 27?
+// alert(num1 + num2)
+
+// Evaluate: 50 - 18
+
+
+// What will 7 * 8 give?
+
+// What is 144 / 12?
+
+// Find the remainder of 29 % 4
+
+// Calculate 3 ** 4
+
+// If let x = 10; x++; what is the value of x?
+// let x = 10;
+// console.log(x++);
+
+// console.log(x);
+
+
+// If let y = 20; y--; what is the value of y?
+
+// What is the result of (5 + 3) * 2?
+
+// // Find the value of 100 % 30
+// let a = 10;
+// console.log(a--);
+// console.log(a);
+// using all arithmetic opertors 
+// let a = Number(prompt("enter the number:"));
+// let b = Number(promp
+// console.log("5"+5);t("enter the number: "));
+// let num3 = `${a+b} $
+// let a = Number(prompt("Enter the number1"));
+// let b = Number(prompt("Enter the number2"));
+// let c = prompt(" which opertion do you want? \n (add, sub, multi, div,mod,expo,inc,dec)");
+// if (c === "add") {
+//     alert(`Addition:${a}+${b}=${a + b}`)
+
+// } else if (c === "sub") {
+//     alert(`Subtraction=${a}-${b}=${a - b}`)
+// } else if (c === "multi") {
+//     alert(`Multiplication=${a}*${b}=${a * b}`)
+// } else if (c==="div"){
+//     alert(`Division=${a}/${b}=${a/b}`)
+// }else if (c==="mod"){
+//     alert(`Modulus=${a}%${b}=${a%b}`)
+// } else if(c==="expo"){
+//     alert(`Exponentiation =${a}**${b}=${a**b}`)
+// } else if(c==="inc"){
+//     alert(`Increment=++${a}=${++a}`)
+// } else if( c==="dec"){
+//     alert(`Decrement=--${b}=${--b}`)
+// }else{
+//     alert(`Invalid operation Please choose a valid option`)
+// }
+
+
+// console.log('19' < 20);
+// console.log([]+'');
+// let age = 2;
+// let hasid=true;
+// if(age>=18 && hasid){
+//     console.log("allowed");
+
+// }else{
+//     console.log("not allowed");
+
+// }
+// let a = 5;
+// let b = 10;
+// if (a > 3 || b < 5) {
+//     console.log("conditiontrue");
+
+// }
+// let israining = false;
+// if (!israining) {
+//     console.log("go outside");
+
+// }
+// let light = "red";
+// let isambulance = false;
+// if (light == "green " || !isambulance) {
+//     console.log("move");
+
+// }
+// Write an if statement to check if:
+
+// username is "admin"
+
+// password is "secure123"
+
+// If both are correct, allow login.
+// let userName = "admin";
+// let password = "secure23"
+// if (userName == "admi" && password == "secure123") {
+//     console.log(" allowlogin");
+
+// }
+// 1)even or oddWrite a program using if–else to check whether a number is even or odd.
+// 
+// let num = Number(prompt("Enter the number"))
+// if(num%2==0){
+//     console.log("Even");
+
+// }else{
+//     console.log("odd");
+
+// }
+// 2)Age Verification
+
+// Ask the user's age.
+// If age is 18 or above, print “Eligible to vote”.
+// Else print “Not eligible”
+// let user= Number(prompt("Enter the age"));
+// if(user>=18){
+//     alert("Eligible")
+// }else{
+//     alert("not eligible")
+// }
+// 3️⃣ Temperature Check
+
+// If temperature is above 30°C, print “It’s hot”.
+// Else print “Normal weather”.
+let temperature = 1;
+if (temperature >= 30) {
+    console.log("it is hot");
+
+
+} else {
+    console.log("normal weather");
+
+
+}
+// 4️⃣ Compare Two Numbers
+
+// Given two numbers a and b:
+// If a is greater, print “a is bigger”.
+// Else print “b is bigger”.
+let a = 7;
+let b = 6;
+if (a > b) {
+    console.log("a is bigger");
+
+
+} else {
+    console.log("b is bigger");
+
+
+}
+// 1️⃣ Login System
+
+// If the user enters the correct password, show “Login Successful”.
+// Else show “Invalid Password”.
+// let userinfo = prompt(" enter the user correct password:");
+// if (userinfo == "manu1") {
+//     alert("loginSuccessful")
+// } else {
+//     alert("invalid Password")
+// }
+// let tamilMovie = "leo"
+// let teluguMovie = "RRR"
+// let ticketPrice = 150
+// let availableSeats = 100
+
+// console.log(`Today's shows include the Tamil movie ${tamilMovie} and the Telugu movie "${teluguMovie}". Each ticket costs ₹${ticketPrice}, and there are ${availableSeats} seats available`);
+
+// let availableSeats =100;
+// let booktickets= 3;
+// let remainingseats = availableSeats-booktickets;
+// console.log(remainingseats);
+
+let availableSeats = 50;
+let booked = 7;
+let remainingseats = availableSeats - booked;
+console.log("Remaining seats:", remainingseats);
+
+// 4. Login System 
+// Create variables: 
+// ● userName = "Arjun" 
+// ● isLoggedIn = false 
+// If logged in, print "Welcome, Arjun". Otherwise, print "Please login".
+
+// let userName = "Arjun"
+// let isloggedIn = false;
+// if (isloggedIn) {
+//     console.log("Welcome, Arjun");
+
+// } else {
+//     console.log("Please login");
+
+// }
+
+// const itemName ="Mobile";
+// let quantity = 1;
+// quantity+=4;
+// console.log(quantity);
+let movie = "RRR"
+{
+    let movie = "bahubali"
+    console.log("innervariable:", movie);
+
+}
+console.log("outervariable:",movie);
+
+
+
