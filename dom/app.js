2ef46773cf80cd6ff6201d3e4a1bbfa47fc3656a
NalaboluManaswini <manaswininalabolu56@gmail.com>
2025-12-02 00:00:56 +0530

    task1

---
+++ dom/app.js
@@ -0,0 +1,19 @@
+// console.log(document);
+// document.body.style.backgroundColor="red"
+// let ul = document.getElementsByTagName('ul')
+// ul[1].style.color="red"
+// console.log(ul);
+// function colorDOM() {
+//     let ul = document.getElementById('child1')
+//     ul.style.color='red'
+
+// }
+// let a= input("enter the number ")
+// let b = input("enter the number2")
+function printresult() {
+    const a = Number(document.getElementById('num1').Value)
+    const b = Number(document.getElementById('num2').Value)
+    const result = a + b;
+    console.log(result);
+
+}
--- task1/app.js
+++ task1/app.js
@@ -1,21 +1,25 @@
 
-// let items = [{
-//     image:" https://images.pexels.com/photos/17706646/pexels-photo-17706646.jpeg",
-//     productName:"Hp laptop",
-//     price:"50k"
-// },
-// {image:"https://images.pexels.com/photos/939331/pexels-photo-939331.jpeg",
-//     productName:"iapple",
-//     price:"1L"
-// },
-// {
-// image:"https://images.pexels.com/photos/7974/pexels-photo.jpg",
-// productName:"Acer Aspire 7",
-// price:"30k"
-// },
-// {
-// image:"https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg",
-// productName:"Samsung Galaxy Book 3",
-// price:"70k"
-// }]
-// for (){}
+let items = [{
+    image:" https://images.pexels.com/photos/17706646/pexels-photo-17706646.jpeg",
+    productName:"Hp laptop",
+    price:"50k"
+},
+{image:"https://images.pexels.com/photos/939331/pexels-photo-939331.jpeg",
+    productName:"iapple",
+    price:"1L"
+},
+{
+image:"https://images.pexels.com/photos/7974/pexels-photo.jpg",
+productName:"Acer Aspire 7",
+price:"30k"
+},
+{
+image:"https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg",
+productName:"Samsung Galaxy Book 3",
+price:"70k"
+}]
+let container = document.getElementById("product")
+for (i=0; i<product.length; i++){
+    let  item = product[i];
+    
+}
